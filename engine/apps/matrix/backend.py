0dfe0f26bfb9ace1fbec58af85e5a344c83625e7
Jack Jackson <scubbojj@gmail.com>
2022-11-19 12:56:23 -0800

    Beginning refactor and cleanup

--- engine/apps/matrix/backend.py
+++ engine/apps/matrix/backend.py
@@ -1,14 +1,9 @@
 from apps.base.messaging import BaseMessagingBackend
-from apps.matrix.tasks import notify_user_via_celery
-from django.conf import settings
 from apps.matrix.alert_rendering import build_message
 
 import asyncio
 
-from apps.user_management.models import User
-
-from nio import AsyncClient, LoginResponse
-from .client import MatrixClient
+from apps.matrix.tasks import notify_user_via_matrix
 
 import logging
 logger = logging.getLogger(__name__)
@@ -21,17 +16,15 @@ class MatrixBackend(BaseMessagingBackend):
     available_for_use = True
 
     # TODO - change these to appropriate values once they're understood
-    templater = "apps.email.alert_rendering.AlertEmailTemplater"
+    templater = "apps.matrix.alert_rendering.AlertMatrixTemplater"
     template_fields = ("title", "message")
+
 
     def serialize_user(self, user):
         return {key: repr(getattr(user, key)) for key in ['user_id', 'name', 'matrix_user_identity']}
 
+
     def notify_user(self, user, alert_group, notification_policy):
-        logger.critical('DEBUG - 0')
-        # notify_user_via_celery(
-        #     user_pk=user.pk, alert_group_pk=alert_group.pk, notification_policy_pk=notification_policy.pk
-        # )
         from apps.base.models import UserNotificationPolicy, UserNotificationPolicyLogRecord
 
         if not user.matrix_user_identity:
@@ -49,51 +42,11 @@ class MatrixBackend(BaseMessagingBackend):
 
         paging_room_id = user.matrix_user_identity.paging_room_id
         message = build_message(alert_group, user.matrix_user_identity)
-        asyncio.run(self.debugging_method(user, alert_group, notification_policy, paging_room_id, message))
+        # The reason for the split in logic between everything above and below this line:
+        # * interactions with Django model object properties (e.g. `user.matrix_user_identity`) cannot
+        #     be carried out in an async context.
+        # * calls to Matrix via matrix-nio are heavily encouraged to be made asynchronously rather
+        #     than synchronously.
+        asyncio.run(notify_user_via_matrix(
+            user, alert_group, notification_policy, paging_room_id, message))
 
-    async def debugging_method(self, user, alert_group, notification_policy, paging_room_id, message):
-        # imported here to avoid circular import error
-        from apps.base.models import UserNotificationPolicy, UserNotificationPolicyLogRecord
-
-        logger.critical(f'DEBUGGG - inside notify_user_async')
-
-        logger.critical(f'DEBUG - 5')
-        logger.critical(f'{paging_room_id}')
-
-        client = await MatrixClient.login_with_username_and_password(
-            settings.MATRIX_USER_ID,
-            settings.MATRIX_PASSWORD,
-            "temporary-grafana-device-id",
-            settings.MATRIX_HOMESERVER
-        )
-        logger.critical(client)
-
-        # if not await client.is_in_room(paging_room_id):
-        if not await client.is_in_room_unmodified(paging_room_id):
-            # TODO - error checking is particularly important here - you can visually check that your user_id
-            # exists and is correct, but you can't check that the bot's able to join a room without actually having it
-            # try to do so.
-            # (To be clear - what is currently here is _probably_ insufficient, but I'm reliant on the Oncall team
-            # to tell me how to raise an "out-of-band" error)
-            logger.critical(f'DEBUG - trying to join room')
-            try:
-                await client.join_room(paging_room_id)
-            except Exception as e:
-                UserNotificationPolicyLogRecord.objects.create(
-                    author=user,
-                    type=UserNotificationPolicyLogRecord.TYPE_PERSONAL_NOTIFICATION_FAILED,
-                    notification_policy=notification_policy,
-                    alert_group=alert_group,
-                    reason="error while sending Matrix message",
-                    notification_step=notification_policy.step,
-                    notification_channel=notification_policy.notify_by
-                )
-                logger.error(f"Unable to join paging_room {paging_room_id} to notify user {user.pk}:")
-                logger.exception(e)
-                return
-
-        logger.critical(f'DEBUG - 6')
-
-        await client.send_message_to_room(
-            paging_room_id,
-            message)
--- engine/apps/matrix/tasks.py
+++ engine/apps/matrix/tasks.py
@@ -15,104 +15,57 @@ from apps.user_management.models import User
 from common.custom_celery_tasks import shared_dedicated_queue_retry_task
 
 
-MAX_RETRIES = 1 if settings.DEBUG else 10
+# MAX_RETRIES = 1 if settings.DEBUG else 10
 logger = get_task_logger(__name__)
+#
+# _client: Optional[MatrixClient] = None
+# client_lock = asyncio.Lock()
+#
+#
+# async def _initialize_client():
+#     global _client
+#     logger.critical(f'Called initialize client')
+#     async with client_lock:
+#         if _client is None:
+#             logger.critical(f'Initializing client')
+#             _client = await MatrixClient.login_with_username_and_password(
+#                 settings.MATRIX_USER_ID,
+#                 settings.MATRIX_PASSWORD,
+#                 "temporary-grafana-device-id",
+#                 settings.MATRIX_HOMESERVER
+#             )
+#             logger.critical(f'==========================================\nFinished initializing client\n==========================================')
+#
+#
+# def get_client():
+#     logger.critical('Called get_client')
+#     global _client
+#     if _client is None:
+#         logger.critical('_client is none inside get_client, starting init task')
+#         asyncio.create_task(_initialize_client())
+#         logger.critical('Init task created')
+#     while _client is None:
+#         logger.critical('Waiting on client')
+#         sleep(1)
+#     return _client
+#
 
-_client: Optional[MatrixClient] = None
-client_lock = asyncio.Lock()
-
-
-async def _initialize_client():
-    global _client
-    logger.critical(f'Called initialize client')
-    async with client_lock:
-        if _client is None:
-            logger.critical(f'Initializing client')
-            _client = await MatrixClient.login_with_username_and_password(
-                settings.MATRIX_USER_ID,
-                settings.MATRIX_PASSWORD,
-                "temporary-grafana-device-id",
-                settings.MATRIX_HOMESERVER
-            )
-            logger.critical(f'==========================================\nFinished initializing client\n==========================================')
-
-
-def get_client():
-    logger.critical('Called get_client')
-    global _client
-    if _client is None:
-        logger.critical('_client is none inside get_client, starting init task')
-        asyncio.create_task(_initialize_client())
-        logger.critical('Init task created')
-    while _client is None:
-        logger.critical('Waiting on client')
-        sleep(1)
-    return _client
-
-
-# @shared_dedicated_queue_retry_task(autoretry_for=(Exception,), retry_backoff=True, max_retries=MAX_RETRIES)
-def notify_user_via_celery(user_pk, alert_group_pk, notification_policy_pk):
-    from apps.base.models import UserNotificationPolicy, UserNotificationPolicyLogRecord
-
-    try:
-        user = User.objects.get(pk=user_pk)
-    except User.DoesNotExist:
-        logger.warning(f"User {user_pk} does not exist")
-        return
-
-    try:
-        alert_group = AlertGroup.all_objects.get(pk=alert_group_pk)
-    except AlertGroup.DoesNotExist:
-        logger.warning(f"Alert group {alert_group_pk} does not exist")
-        return
-
-    try:
-        notification_policy = UserNotificationPolicy.objects.get(pk=notification_policy_pk)
-    except UserNotificationPolicy.DoesNotExist:
-        logger.warning(f"User notification policy {notification_policy_pk} does not exist")
-        return
-
-    if not user.matrix_user_identity:
-        UserNotificationPolicyLogRecord.objects.create(
-            author=user,
-            type=UserNotificationPolicyLogRecord.TYPE_PERSONAL_NOTIFICATION_FAILED,
-            notification_policy=notification_policy,
-            alert_group=alert_group,
-            reason="Error while sending Matrix message",
-            notification_step=notification_policy.step,
-            notification_channel=notification_policy.notify_by
-        )
-        logger.error(f"Error while sending Matrix message - no matrix_user_identity for user {user.pk}")
-        return
-
-    # Unfortunately, interaction with Matrix via nio is predominantly via async functions,
-    # and it turns out that multiple nested calls to `asyncio.get_event_loop().run_until_complete(`
-    # can cause calls to hang, so we partition the logic off in a single async function wherein
-    # `await` can be used instead.
-    #
-    # (Conversely, calls to, e.g., `User.objects.get` can only be called from a synchronous context,
-    # hence why we can't move _all_ the logic into a single async function)
-    event_loop = asyncio.get_event_loop()
-    logger.critical(f'{event_loop=}')
-    logger.critical(f'{event_loop.is_running()=}')
-    asyncio.get_event_loop().run_until_complete(notify_user_async(user, alert_group, notification_policy))
-
-
-async def notify_user_async(user, alert_group, notification_policy):
+@shared_dedicated_queue_retry_task(autoretry_for=(Exception,), retry_backoff=True, max_retries=1)
+async def notify_user_via_matrix(user, alert_group, notification_policy, paging_room_id, message):
     # imported here to avoid circular import error
     from apps.base.models import UserNotificationPolicy, UserNotificationPolicyLogRecord
 
     logger.critical(f'DEBUGGG - inside notify_user_async')
 
-    identity = user.matrix_user_identity
-    paging_room_id = identity.paging_room_id
-
     logger.critical(f'DEBUG - 5')
     logger.critical(f'{paging_room_id}')
 
-    logger.critical(f'Client is {_client}')
-    client = get_client()
-    logger.critical(client)
+    client = await MatrixClient.login_with_username_and_password(
+        settings.MATRIX_USER_ID,
+        settings.MATRIX_PASSWORD,
+        "temporary-grafana-device-id",
+        settings.MATRIX_HOMESERVER
+    )
 
     # if not await client.is_in_room(paging_room_id):
     if not await client.is_in_room_unmodified(paging_room_id):
@@ -121,7 +74,6 @@ async def notify_user_async(user, alert_group, notification_policy):
         # try to do so.
         # (To be clear - what is currently here is _probably_ insufficient, but I'm reliant on the Oncall team
         # to tell me how to raise an "out-of-band" error)
-        logger.critical(f'DEBUG - trying to join room')
         try:
             await client.join_room(paging_room_id)
         except Exception as e:
@@ -142,4 +94,4 @@ async def notify_user_async(user, alert_group, notification_policy):
 
     await client.send_message_to_room(
         paging_room_id,
-        build_message(alert_group, identity.user_id))
+        message+' - iteration 3, with rename')
